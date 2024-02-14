cfa7f3e68c9539b4304a276f1441b4329b0d0e54
lrdxd <mikhold06@gmail.com>
2024-02-14 17:32:50 +0300

    задание 4

--- space_4.py
+++ space_4.py
@@ -1,5 +1,24 @@
 import csv
+import string
+#выдача кораблю пароля
+def credentials(s: string):
+    ship = s.split()
+    return f'{ship[0]}_{ship[1][0]}{ship[2][0]}'
 
+#список кораблей с паролями
+ship_passes = []
+
+#проход по файлу и присвоение пароля
+with open("space.csv", encoding='utf-8') as file:
+    reader = list(csv.DictReader(file, delimiter='*', quotechar='"'))
+    for row in reader:
+        row['password']=credentials(row['ShipName'])
+
+#запись нового файла
+with open("space_uniq_password.csv", 'w', newline='', encoding='utf-8') as file:
+    w = csv.DictWriter(file, fieldnames=['ShipName','planet','coord_place','direction', 'password'])
+    w.writeHeader()
+    w.writerows(shis_passes)
 
 
 
